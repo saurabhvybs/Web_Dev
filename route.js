19df7bfe29519961cc32d947a27f47e7cb5f83dc
Saurabh Pandey <saurabhpandey@Saurabhs-MacBook-Air.local>
2024-05-30 01:37:50 +0530

    made an zod authentication function

--- route.js
+++ route.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express();
-
+// whenever we give a function or middleware in app.use it simple runs at the first then control reaches else.
 app.use(express.json()); // a simple middleware to parse the input into a json 
 
 app.post("/health-checkup", function (req, res) {
@@ -10,6 +10,7 @@ app.post("/health-checkup", function (req, res) {
   res.send("you have " + len + " kidneys");
 });
 // Global Catches to deal with the error msg
+//any wrong input will hit this middleware 
 app.use(function(err ,req ,res ,next){
   res.json({
     msg: "something is up with our server"
---
+++ zod.js
@@ -0,0 +1,14 @@
+const zod = require("zod");
+
+function validateInput(obj){
+    const schema = zod.object({
+        email:zod.string().email(),
+        password:zod.string().min(8)
+})
+const response = schema.safeParse(obj);
+console.log(response);
+}
+validateInput({
+    email: "saurabh@gmail.com",
+    password: "1234456788"
+})
